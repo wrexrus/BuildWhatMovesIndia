b045fb6bf29a15f849085fbad39feef745162dc6
rushabh wagh <rushabhwagh125@gmail.com>
2026-08-23 20:24:10 +0530

    re:backend

--- backend/src/routes/pwdRoutes.js
+++
@@ -1,9 +0,0 @@
-// PwD Applicant Portal Routes
-const express = require('express');
-const router = express.Router();
-const { pwdLogin, getPwdDashboard } = require('../controllers/pwdAuthController');
-
-router.post('/auth/login', pwdLogin);
-router.get('/dashboard', getPwdDashboard);
-
-module.exports = router;
